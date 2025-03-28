0e7b8c4dedfcf984eaf2e3bc6897f97405b7d43c
Hammad1246 <hammadsaleem1246@gmail.com>
2025-03-28 09:29:35 +0500

    Update CORS configuration and add Vercel configuration file

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(
   cors({
-    origin:'http://localhost:5173',
+    origin:['https://todo-app-server-zeta-sage.vercel.app ', 'http://localhost:5173'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
